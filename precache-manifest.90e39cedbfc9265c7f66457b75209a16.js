bc323f39c1d4ce785894f5fb7061e142adf5d257
Somil Bichpuriya <57514538+sbichpuriya11@users.noreply.github.com>
2022-07-19 10:25:45 +0530

    Updates

---
+++ precache-manifest.90e39cedbfc9265c7f66457b75209a16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5ce89f5302ca34a0e02844c783bd80",
+    "url": "/e-commerce-app/index.html"
+  },
+  {
+    "revision": "f6b997c691e878ed5b99",
+    "url": "/e-commerce-app/static/css/main.5079fc5d.chunk.css"
+  },
+  {
+    "revision": "8fa9116213b6e8eab9a8",
+    "url": "/e-commerce-app/static/js/2.f65f1d26.chunk.js"
+  },
+  {
+    "revision": "a712f5023b98f1ee6f879156125341ec",
+    "url": "/e-commerce-app/static/js/2.f65f1d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b997c691e878ed5b99",
+    "url": "/e-commerce-app/static/js/main.87234cdd.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/e-commerce-app/static/js/main.87234cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff831d48999367313649",
+    "url": "/e-commerce-app/static/js/runtime-main.4dd6464e.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/e-commerce-app/static/media/commerce.422fec10.png"
+  }
+]);
